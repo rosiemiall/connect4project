18169ac643a74d522f97fabdea092d8148aceeeb
Rosie Miall <rosiemiall38@gmail.com>
2024-07-10 17:41:39 +0100

    adds the properties of the game board and the constructors.

--- connect4_server/src/main/java/com/example/connect4/models/Gameboard.java
+++ connect4_server/src/main/java/com/example/connect4/models/Gameboard.java
@@ -1,4 +1,47 @@
 package com.example.connect4.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+//
+//import jakarta.persistence.*;
+//
+//@Entity
+//@Table(name = "games")
 public class Gameboard {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+
+    private List<List<Piece>> columns;
+    private int numberOfRows;
+
+    public Gameboard() {
+    }
+
+    public Gameboard(int numberOfColumns, int numberOfRows) {
+        this.columns = new ArrayList<>();
+        this.numberOfRows = numberOfRows;
+
+//        add an empty list for the number of columns needed
+        for (int i = 0; i < numberOfColumns; i++){
+            this.columns.add(new ArrayList<>());
+        }
+    }
+
+    public List<List<Piece>> getColumns() {
+        return columns;
+    }
+
+    public void setColumns(List<List<Piece>> columns) {
+        this.columns = columns;
+    }
+
+    public int getNumberOfRows() {
+        return numberOfRows;
+    }
+
+    public void setNumberOfRows(int numberOfRows) {
+        this.numberOfRows = numberOfRows;
+    }
 }
